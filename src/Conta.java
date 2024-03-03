094247946b6d4f63dfead6c353592fa3e3c17883
mariocb-duarte <mcbd.eng@gmail.com>
2024-03-03 10:36:05 -0300

    update

---
+++ src/Conta.java
@@ -0,0 +1,56 @@
+public class Conta implements IConta {
+    private static final int AGENCIA_PADRAO = 1;
+    private static int SEQUENCIAL = 1;
+
+    protected int agencia;
+    protected int numero;
+    protected double saldo;
+    protected Cliente cliente;
+
+    public Conta(Cliente cliente) {
+        this.agencia = Conta.AGENCIA_PADRAO;
+        this.numero = SEQUENCIAL++;
+        this.cliente = cliente;
+    }
+
+    @Override
+    public void sacar(double valor) {
+        saldo -= valor;
+    }
+
+    @Override
+    public void depositar(double valor) {
+        saldo += valor;
+    }
+
+    @Override
+    public void transferir(double valor, IConta contaDestino) {
+        this.sacar(valor);
+        contaDestino.depositar(valor);
+    }
+
+    @Override
+    public void imprimirExtrato() {
+
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void imprimirInfosComuns() {
+        System.out.printf("Cliente: %s%n", this.cliente.getNome());
+        System.out.printf("Agencia: %d%n", this.agencia);
+        System.out.printf("Numero: %d%n", this.numero);
+        System.out.printf("Saldo: %.2f%n", this.saldo);
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Cliente mario = new Cliente();
+
+        mario.setNome("Mario");
+
+        Conta cc = new ContaCorrente(mario);
+        Conta poupanca = new ContaPoupanca(mario);
+
+        cc.depositar(100);
+        cc.imprimirExtrato();
+        poupanca.imprimirExtrato();
+        cc.depositar(1000);
+        cc.imprimirExtrato();
+
+
+    }
+}
